bc6e8d3f63d2feaf4dd611ceec469139556eab59
elshafei-developer <el.shafei.developer@gmail.com>
2024-02-14 14:27:59 +0300

    Verification exists Employee and ID Employee

--- my_hrms/my_hrms/doctype/pio_id/pio_id.js
+++ my_hrms/my_hrms/doctype/pio_id/pio_id.js
@@ -1,7 +1,18 @@
 // Copyright (c) 2024, hassan hussein mohammed and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("PIO ID", {
-//   refresh(frm) {
-//   },
-// });
+frappe.ui.form.on("PIO ID", {
+  refresh(frm) {},
+  before_save(frm) {},
+});
+frappe.realtime.on("my_custom_channel", function (data) {
+  frappe.confirm(
+    "The ID is already in use, do you want to update the information ?",
+    () => {
+      // action to perform if Yes is selected
+    },
+    () => {
+      // action to perform if No is selected
+    }
+  );
+});
--- my_hrms/my_hrms/doctype/pio_id/pio_id.py
+++ my_hrms/my_hrms/doctype/pio_id/pio_id.py
@@ -2,28 +2,25 @@
 # For license information, please see license.txt
 
 import frappe
-from zk import ZK, const
+from frappe import _
+from zk import ZK
 from frappe.model.document import Document
 
 
 class PIOID(Document):
 	def validate(self):
-     
-		devices_ZK = frappe.db.get_list("ZK Device", fields=["location_device","id_device"])
-		for device in devices_ZK:
-			print("#"*40)
-			print("-"*40)
-			print(f"ID: {id}")
-			if(connect_zk(self.id_employee_in_device,device)):
-				frappe.throw(f"User ID found in device {device['location_device']} please choose another ID")
-			# connect_zk(self.id_employee_in_device,device)
-			print("-"*40)
-			print("#"*40)
+		if self.id_employee_in_device:
+			self.fingerprints = self.id_employee_in_device
+	def before_insert(self):
+		employees = frappe.db.get_list("PIO ID", pluck="employee")
+		exists_employee = frappe.db.exists({"doctype": "PIO ID", "id_employee_in_device": self.id_employee_in_device})
   
-  
+		if self.employee in employees:
+			frappe.throw(f"Employee {self.full_name} already exists")
+		if exists_employee:
+			frappe.throw(f"The Employee {exists_employee} has ID {self.id_employee_in_device} already")
+			
 def connect_zk(employee_id,device):
-    
-    
 	conn = None
 	zk = ZK(device['id_device'])
 	try:
@@ -33,9 +30,16 @@ def connect_zk(employee_id,device):
 		for user in users:
 			if(user.user_id == employee_id):
 				return True
-
 		conn.test_voice()
 		conn.enable_device()
 	except Exception as e:
-		# frappe.msgprint(f"Error:")
+		# return False
 		pass
+
+# def send_data_to_js(data):
+# 	# Publish data to the specified channel
+# 	frappe.publish_realtime('my_custom_channel', data)
+ 
+# @frappe.whitelist()
+# def move_to_custom_doctype(name,ID):
+#     return {"message": f"The Employee {name} has ID {ID} Do you want update information this Employee ?"}
